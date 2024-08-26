6e602eaeb282e8f84c34667c6e73141b61b3355b
keimha <keimham@yahoo.com>
2024-08-26 12:17:31 -0500

    FirstJavaProgram

---
+++ FirstJavaP.java
@@ -0,0 +1,15 @@
+public class FirstJavaP {
+	public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo\r\n" + //
+                        "I am 100 years old\r\n" + //
+                        "My hometown is Burbank, CA\r\n" + //
+                        "\r\n" + //
+                        "");
+
+	}
+}
+
+
+// to compile a code use javac FirstJavaP.java
+// to run code use java FirstJavaP.java
+//when yo make changes to your code always remeber to re compile and run!
